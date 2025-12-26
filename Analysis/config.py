717572738231cd39aab7b81903b6f83dceb0becd
MarionGuelfand <marion.guelfand@lpnhe.in2p3.fr>
2025-12-26 20:18:12 +0100

    Add txt files for example

--- Analysis/config.py
+++ Analysis/config.py
@@ -7,22 +7,35 @@ import os
 # --------------------------
 # Paths
 # --------------------------
-base_dir = '/Users/mguelfan/Documents/GRAND/ADF_DC2/'
-data_dir = base_dir + 'output_recons_DC2/CR_candidates/adc_candidates_Jolan/'
-output_figures = data_dir + 'Figures_02:12:2025/'
-path_antenna_position = '/Users/mguelfan/Documents/GRAND/ADF_DC2/output_recons_DC2/CR_candidates/'
+base_dir = os.path.dirname(__file__) 
+use_examples= True 
+if use_examples:
+    data_dir = os.path.join(base_dir, "examples")
+    ANTENNA_FILE = os.path.join(data_dir, "_gp13_65_rtksort.txt")
+    root_paths = [
+        os.path.join(data_dir, 'August_2025'),
+        os.path.join(data_dir, 'September_2025')
+    ]
+    month_names = [ 'August', 'September']
 
-# Antenna file
-ANTENNA_FILE = path_antenna_position + '_gp13_65_rtksort.txt'
 
-# Paths for months
-root_paths = [
-    data_dir + 'July_2025/causality/GLOBAL_filtered/',
-    data_dir + 'August_2025/causality/GLOBAL_filtered/',
-    data_dir + 'September_2025/causality/GLOBAL_filtered/',
-    data_dir + 'October_2025/causality/GLOBAL_filtered/'
-]
-month_names = ['July', 'August', 'September', 'October']
+else:
+    base_dir = '/Users/mguelfan/Documents/GRAND/ADF_DC2/'
+    data_dir = base_dir + 'output_recons_DC2/CR_candidates/adc_candidates_Jolan/'
+    output_figures = data_dir + 'Figures_02:12:2025/'
+    path_antenna_position = '/Users/mguelfan/Documents/GRAND/ADF_DC2/output_recons_DC2/CR_candidates/'
+
+    # Antenna file
+    ANTENNA_FILE = path_antenna_position + '_gp13_65_rtksort.txt'
+
+    # Paths for months
+    root_paths = [
+        data_dir + 'July_2025/causality/GLOBAL_filtered/',
+        data_dir + 'August_2025/causality/GLOBAL_filtered/',
+        data_dir + 'September_2025/causality/GLOBAL_filtered/',
+        data_dir + 'October_2025/causality/GLOBAL_filtered/'
+    ]
+    month_names = ['July', 'August', 'September', 'October']
 
 # Colors for plots
 colors = plt.cm.coolwarm(np.linspace(0, 1, len(month_names)))
